defaef5942dd78dc1329fc3945a42c24ca775816
thandananipenta <thandanani@pentaholdings.co.za>
2022-08-30 16:22:49 +0200

    dfgdfgdfg

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero  } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,12 +11,9 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  //hero = 'Windstorm';
-  hero: Hero = { id: 1, name: 'windstorm'};
-  
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService,private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -24,11 +23,9 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected hero id=$'+hero.id)
   }
-  // getHeroes(): void {
-  //   this.heroes = this.heroService.getHeroes();
-  // }
-
+  
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
